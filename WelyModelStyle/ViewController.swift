6040043f814b192e19ed5251be1b7e7242311012
yangweiyi <3142107409@qq.com>
2020-08-01 16:53:31 +0800

    修改 测试问题

--- WelyModelStyle/ViewController.swift
+++ WelyModelStyle/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemPink
         // Do any additional setup after loading the view.
     }
 
